2e19ba7b1b8e301cbd820a1e1fd88492fe663e56
Paul Guaman <guaman.bryan.paul@gmail.com>
2020-10-04 20:19:04 -0500

    build

---
+++ build/precache-manifest.8b4728990db3a928e9c8be255c4b14a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56ff36f3b2f9e0d4fa820893918c5b5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53bbca13fd3f38f123d4",
+    "url": "/static/css/main.5181734c.chunk.css"
+  },
+  {
+    "revision": "acc6308324a026721788",
+    "url": "/static/js/2.49b8322f.chunk.js"
+  },
+  {
+    "revision": "d56f7d70e49a582c3cb88e84fa36367e",
+    "url": "/static/js/2.49b8322f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53bbca13fd3f38f123d4",
+    "url": "/static/js/main.be7dc979.chunk.js"
+  },
+  {
+    "revision": "12f12c6e8a6accca6b3a",
+    "url": "/static/js/runtime-main.24c8bc31.js"
+  }
+]);
